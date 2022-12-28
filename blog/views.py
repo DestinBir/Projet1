35db4df63f6a3711bdb0c6554a9e20cc7d4251b3
Biringanine Destin <113047575+DestinBir@users.noreply.github.com>
2022-12-28 23:10:00 +0200

    Changes to be committed: 	modified:   .idea/Projet1.iml 	new file:   .idea/misc.xml 	modified:   Projet1/__pycache__/urls.cpython-311.pyc 	modified:   Projet1/urls.py 	deleted:    blog/__pycache__/urls.cpython-311.pyc 	modified:   blog/__pycache__/views.cpython-311.pyc 	modified:   blog/templates/blog.html 	modified:   blog/views.py 	modified:   db.sqlite3 	modified:   templates/index.html 	deleted:    users/__pycache__/urls.cpython-311.pyc 	modified:   users/__pycache__/views.cpython-311.pyc 	modified:   users/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def say_hello(request):
+def index(request):
     
-    return render(request, 'blog.html', {'name':'Destin', 'age':19})
+    return render(request, 'blog.html')
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from users.models import User
+
+
 def signIn(request):
-    
     return HttpResponse("Inscription")
 
-def user(request):
 
-    return HttpResponse("Je suis un utilistaeur")
+def user(request, slug):
+    current_user = get_object_or_404(User, name=slug)
+    return HttpResponse('je suis '+f'{current_user.name} {current_user.email}')
